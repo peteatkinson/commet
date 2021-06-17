f9f0d7a291822df255236b7c470a1d440ed6f466
Peter Atkinson <peteratkinson1994@gmail.com>
2021-06-17 23:50:43 +0100

    Update formatting on schema file

--- db/20210616000622-init-schema.sql
+++ db/20210616000622-init-schema.sql
@@ -1,34 +1,34 @@
 CREATE TABLE IF NOT EXISTS discussions (
-  discussionId            uuid         NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(),
-  domain                  TEXT         NOT NULL,
-  page                    TEXT         NOT NULL,
-  ownerId                 uuid         NOT NULL DEFAULT uuid_generate_v4(),                        
-  state                   TEXT         NOT NULL, 
-  lastEventId             uuid         NOT NULL,
-  modifiedAt              TIMESTAMP    NOT NULL,
-  createdAt               TIMESTAMP    NOT NULL
+  discussionId            uuid          NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(),
+  domain                  TEXT          NOT NULL,
+  page                    TEXT          NOT NULL,
+  ownerId                 uuid          NOT NULL DEFAULT uuid_generate_v4(),                        
+  state                   TEXT          NOT NULL, 
+  lastEventId             uuid          NOT NULL,
+  modifiedAt              TIMESTAMP     NOT NULL,
+  createdAt               TIMESTAMP     NOT NULL
 );
 
 CREATE UNIQUE INDEX discussionsIndex oN discussions(discussionId, ownerId)
 
 CREATE TABLE commentOwners (
-	ownerId                uuid          NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate(),
-	ipAddress              TEXT          NOT NULL,
-	privacyMode            TEXT          NOT NULL DEFAULT "anonymous"
+	ownerId                 uuid          NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate(),
+	ipAddress               TEXT          NOT NULL,
+	privacyMode             TEXT          NOT NULL DEFAULT "anonymous"
 )
 
 CREATE UNIQUE INDEX commentOwnersIndex on commentOwners(ownerId, ipAddress)
 
 CREATE TABLE IF NOT EXISTS comments (
-	commentId              uuid          NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(),
-	discussionId           uuid          NOT NULL,
-	html                   TEXT          NOT NULL,
-	markdown               TEXT          NOT NULL,
-	ownerId                uuid          not NULL,
-	state                  TEXT          NOT NULL,
-	createdAt              TIMESTAMP     NOT NULL DEFAULT uuid_generate_v4(),
-	lastEventId            uuid          NOT NULL,
-	modifiedAt             TIMESTAMP     NOT NULL
+  commentId               uuid          NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(),
+  discussionId            uuid          NOT NULL,
+  html                    TEXT          NOT NULL,
+  markdown                TEXT          NOT NULL,
+  ownerId                 uuid          NOT NULL,
+  state                   TEXT          NOT NULL,
+  createdAt               TIMESTAMP     NOT NULL DEFAULT uuid_generate_v4(),
+  lastEventId             uuid          NOT NULL,
+  modifiedAt              TIMESTAMP     NOT NULL
 )
 
 CREATE UNIQUE INDEX commentsIndex ON comments(commentId, discussionId);
